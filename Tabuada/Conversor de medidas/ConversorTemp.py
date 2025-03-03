7a2ee090c65e10320aed52047d489b7226aaf73e
ysaewlustosa <51036941+ysaewlustosa@users.noreply.github.com>
2025-03-03 19:33:03 -0300

    @

---
+++ Tabuada/Conversor de medidas/ConversorTemp.py
@@ -0,0 +1,12 @@
+print("Conversor de temperatura")
+
+temp=float(input("Digite a temperatura que deseja converter: "))
+
+Fahrenheit = (temp * 9/5) + 32
+
+print("A temperatura em Fahrenheit é: ", Fahrenheit)
+
+
+
+
+
